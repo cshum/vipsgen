fb885d11275797bd1525e25de0e496c78f7246a1
Adrian Shum <adrian@cshum.com>
2025-05-12 16:36:49 +0800

    cleanup excluded operations

--- config.go
+++ config.go
@@ -4,13 +4,4 @@ package vipsgen
 var OperationConfigs = map[string]OperationConfig{}
 
 // ExcludedOperations defines operations that should be excluded from generation
-var ExcludedOperations = map[string]bool{
-	// Internal or deprecated operations
-	"cache":   true,
-	"system":  true,
-	"version": true,
-
-	// Operations that require special handling
-	"sequential": true, // Usually handled via loader options
-	"tilecache":  true, // Usually handled internally
-}
+var ExcludedOperations = map[string]bool{}
