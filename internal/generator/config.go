6fda89080e3ccf2067637a8752ba0b6e68d89115
Adrian Shum <adrian@cshum.com>
2025-05-15 10:47:49 +0800

    cleanup excluded operations

--- internal/generator/config.go
+++ internal/generator/config.go
@@ -4,22 +4,4 @@ package generator
 var OperationConfigs = map[string]OperationConfig{}
 
 // ExcludedOperations defines operations that should be excluded from generation
-var ExcludedOperations = map[string]bool{
-	// Exclude custom implementations
-	"jpegsave_buffer":   true,
-	"jpegsave":          true,
-	"heifsave_buffer":   true,
-	"heifsave":          true,
-	"tiffsave_buffer":   true,
-	"tiffsave":          true,
-	"webpsave_buffer":   true,
-	"webpsave":          true,
-	"gifsave_buffer":    true,
-	"gifsave":           true,
-	"jp2ksave_buffer":   true,
-	"jp2ksave":          true,
-	"pngsave_buffer":    true,
-	"pngsave":           true,
-	"magicksave_buffer": true,
-	"magicksave":        true,
-}
+var ExcludedOperations = map[string]bool{}
