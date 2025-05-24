ac1d213ee417db6f9c943d5b35b1498112f582f5
Adrian Shum <adrian@cshum.com>
2025-05-25 01:13:55 +0800

    feat: add image header methods (#29)
    * fix: standardize return of RemoveICCProfile
    
    * build: update generated vips package
    
    * fix: standardize return of RemoveICCProfile
    
    * build: update generated vips package
    
    * fix: standardize return of RemoveICCProfile
    
    * build: update generated vips package
    
    * fix: standardize return of RemoveICCProfile
    
    * build: update generated vips package
    
    * fix: standardize return of RemoveICCProfile
    
    * build: update generated vips package
    
    * fix: standardize return of RemoveICCProfile
    
    * build: update generated vips package
    
    * SetPages
    
    * SetPages
    
    * build: update generated vips package

--- vips/vips.h
+++ vips/vips.h
@@ -822,17 +822,48 @@ int vipsgen_image_new_from_buffer(const void *buf, size_t len, VipsImage **out);
 int vipsgen_image_new_from_buffer_with_option(const void *buf, size_t len, VipsImage **out, const char *option_string);
 int vipsgen_image_new_from_memory(const void *buf, size_t len, int width, int height, int bands, VipsImage **out);
 
+int has_alpha_channel(VipsImage *image);
+int is_colorspace_supported(VipsImage *in);
+void clear_image(VipsImage **image);
+
+unsigned long has_icc_profile(VipsImage *in);
+unsigned long get_icc_profile(VipsImage *in, const void **data,
+                              size_t *dataLength);
 int remove_icc_profile(VipsImage *in);
+
+unsigned long has_iptc(VipsImage *in);
+char **image_get_fields(VipsImage *in);
+const char * get_meta_string(const VipsImage *image, const char *name);
+void image_set_string(VipsImage *in, const char *name, const char *str);
+unsigned long image_get_string(VipsImage *in, const char *name,
+                               const char **out);
+unsigned long image_get_as_string(VipsImage *in, const char *name, char **out);
+
+void remove_field(VipsImage *in, char *field);
+
 int get_meta_orientation(VipsImage *in);
+void remove_meta_orientation(VipsImage *in);
+void set_meta_orientation(VipsImage *in, int orientation);
 int get_image_n_pages(VipsImage *in);
+void set_image_n_pages(VipsImage *in, int n_pages);
 int get_page_height(VipsImage *in);
 void set_page_height(VipsImage *in, int height);
 int get_meta_loader(const VipsImage *in, const char **out);
 void set_image_delay(VipsImage *in, const int *array, int n);
-const char * get_meta_string(const VipsImage *image, const char *name);
+int get_background(VipsImage *in, double **out, int *n);
+
+void image_set_blob(VipsImage *in, const char *name, const void *data,
+                    size_t dataLength);
+unsigned long image_get_blob(VipsImage *in, const char *name, const void **data,
+                             size_t *dataLength);
+
+void image_set_double(VipsImage *in, const char *name, double i);
+unsigned long image_get_double(VipsImage *in, const char *name, double *out);
+
+void image_set_int(VipsImage *in, const char *name, int i);
+unsigned long image_get_int(VipsImage *in, const char *name, int *out);
+
 int remove_exif(VipsImage *in, VipsImage **out);
-int has_alpha_channel(VipsImage *image);
-void clear_image(VipsImage **image);
 
 int embed_multi_page(VipsImage *in, VipsImage **out, int left, int top, int width,
                 int height, int extend);
